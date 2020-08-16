f37c92e01577a54fe216a2e6efb08f32348373a7
bhansa <bsaraswat45@gmail.com>
2020-08-16 16:53:04 +0530

    adding frontend folder, bootstraping react project

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
+
+// TODOs
+// 1. Create dummy job data
+// 2. Create Job Dashboard
+// 3.
